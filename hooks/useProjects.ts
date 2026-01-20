e7a1fdbcf2301cac6b47ccbf305fe9f6ac1e1a89
OIAI <omniaintelligenceteam@gmail.com>
2026-01-19 18:38:45 -0700

    Import uploadImage from uploadService

--- hooks/useProjects.ts
+++ hooks/useProjects.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
+import { uploadImage } from '../services/uploadService';
 import { useUser } from '@clerk/clerk-react';
 import { SavedProject, QuoteData, BOMData } from '../types';
 
